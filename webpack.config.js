eba77b38dc9a047c3057f51d8f9c130aef4865e0
Vilayat Safarov <vilayat571@gmail.com>
2024-05-28 11:51:08 -0700

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,25 +5,9 @@ rules: [{
       {
         loader: 'image-webpack-loader',
         options: {
-          mozjpeg: {
-            progressive: true,
-          },
-          // optipng.enabled: false will disable optipng
-          optipng: {
-            enabled: false,
-          },
-          pngquant: {
-            quality: [0.65, 0.90],
-            speed: 4
-          },
-          gifsicle: {
-            interlaced: false,
-          },
-          // the webp option will enable WEBP
-          webp: {
-            quality: 75
-          }
-        }
+          bypassOnDebug: true, // webpack@1.x
+          disable: true, // webpack@2.x and newer
+        },
       },
     ],
   }]
